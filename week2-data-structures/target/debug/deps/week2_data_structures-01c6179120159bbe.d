46729ba588742b0339912f78603a04c44c154b2c
NickyBenz <nikhil.bezwada07@gmail.com>
2025-05-26 23:29:52 +0800

    Removed unneccessary workflows

---
+++ week2-data-structures/target/debug/deps/week2_data_structures-01c6179120159bbe.d
@@ -0,0 +1,5 @@
+/Users/nikhilbezwada/Desktop/Rust Coursera/tmp/rust-basic-template/CI-Lab/week2-data-structures/target/debug/deps/libweek2_data_structures-01c6179120159bbe.rmeta: src/main.rs
+
+/Users/nikhilbezwada/Desktop/Rust Coursera/tmp/rust-basic-template/CI-Lab/week2-data-structures/target/debug/deps/week2_data_structures-01c6179120159bbe.d: src/main.rs
+
+src/main.rs:
---
+++ week2-data-structures/target/debug/deps/week2_data_structures-76a42bb4894ceb11.d
@@ -0,0 +1,5 @@
+/Users/nikhilbezwada/Desktop/Rust Coursera/tmp/rust-basic-template/CI-Lab/week2-data-structures/target/debug/deps/libweek2_data_structures-76a42bb4894ceb11.rmeta: src/main.rs
+
+/Users/nikhilbezwada/Desktop/Rust Coursera/tmp/rust-basic-template/CI-Lab/week2-data-structures/target/debug/deps/week2_data_structures-76a42bb4894ceb11.d: src/main.rs
+
+src/main.rs:
